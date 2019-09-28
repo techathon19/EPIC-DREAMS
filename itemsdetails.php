273ae6edde58e4ff7239dea146e46420d61bac68
dineshsha2000 <40859065+dineshsha2000@users.noreply.github.com>
2019-09-28 15:05:21 +0530

    THis code is used to get the item details

---
+++ itemsdetails.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <form method="post" action="addtocart.php">
+        Product ID:
+        <input type="text" name="pid">
+        <br>
+        name:
+        <input type="text" name="name">
+        <br>
+
+        cost:
+        <input type="text" name="cost">
+                <br>
+
+        Photo:
+        <input type="file" name="pho">
+        <br>
+        
+        <button type="submit" >add to display</button>
+        
+    </form>
+</body>
+</html>
